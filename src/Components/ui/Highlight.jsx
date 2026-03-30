c59d47033c81ced40e09605830239bd3dd691405
Michelle Anderson <manderson0102@yahoo.com>
2026-03-30 15:52:25 -0400

    updates

--- src/Components/ui/Highlight.jsx
+++ src/Components/ui/Highlight.jsx
@@ -1,17 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-const Highlight = ({ icon }) => {
-    return (
-         <div className="highlight">
-                <div classname="highlight__img">
-                   {icon}
-                </div>
-                <h3 className="highlight__subtitle">Easy and Quick</h3>
-              <p className="highlight__para">
-                  Get access to the book you purchased online instantly.
-              </p>
-           </div>
-    );
-}
+const Highlight = ({ icon, title, para }) => {
+  return (
+    <div className="highlight">
+      <div className="highlight__img">
+        <FontAwesomeIcon icon={icon} />
+      </div>
+      <h3 className="highlight__subtitle">{title}</h3>
+      <p className="highlight__para">{para}</p>
+    </div>
+  );
+};
 
 export default Highlight;
