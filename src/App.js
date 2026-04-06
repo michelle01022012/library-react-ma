f7a3f8f10b03fd127722d49fa891dbc0c9b5ffb3
Michelle Anderson <manderson0102@yahoo.com>
2026-04-06 12:52:21 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Highlights from './Components/Highlights';
 import Landing from './Components/Landing';
 import Nav from './Components/Nav';
+import Featured from './Components/Featured';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
