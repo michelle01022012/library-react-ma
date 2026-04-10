dac86519acc4edd24e7040db576812433a9f0048
Michelle Anderson <manderson0102@yahoo.com>
2026-04-10 12:56:33 -0400

    app.js fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import Footer from "./Components/Footer";
-import Nav from "./Components/Nav";
-import {BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./Components/Home";
-import Books from "./Components/Books";
-
+import "./App.css";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Books from "./components/Books";
 
 function App() {
   return (
-   <Router>
-    <div className="App">
-      <Nav />
-       <Route path="/" exact Component={Home} />
-       <Route path="/books" Component={Books} />
-      <Home />     
-      <Footer />
-    </div>
-  </Router>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" component={Books} />
+        <Home />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { books } from "../data"
+import React from "react";
+import { books } from "../data";
+import Book from "./Book";
 
 const Books = () => {
   return (
     <div id="books__body">
-     <main id="books__main">
+      <main id="books__main">
         <section>
           <div className="books__container">
             <div className="row">
               <div className="books__header"></div>
               <h2 class="section__title books__header--title">All Books</h2>
               <select id="filter">
-                <option value="" selected disabled>Sort</option>
+                <option value="" selected disabled>
+                  Sort
+                </option>
                 <option value="LOW_TO_HIGH">Price Low to High</option>
                 <option value="HIGH_TO_LOW">Price High to Low</option>
                 <option value="RATING">Rating</option>
@@ -19,14 +22,14 @@ const Books = () => {
             </div>
             <div className="books">
               {books.map((book) => (
-              <Book book={book} key={book.id} />
+                <Book book={book} key={book.id} />
               ))}
             </div>
           </div>
         </section>
-     </main>
-   </div>
+      </main>
+    </div>
   );
-}
+};
 
 export default Books;
