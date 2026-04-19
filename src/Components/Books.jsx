d15361dde64e08bc7d09562b3992919772e2579c
Michelle Anderson <manderson0102@yahoo.com>
2026-04-19 11:22:04 -0400

    commit

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -26,16 +26,17 @@ const Books = ({ books: initialBooks }) => {
         <section>
           <div className="books__container">
             <div className="row">
-              <div className="books__header"></div>
-              <h2 className="section__title books__header--title">All Books</h2>
-              <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
-                <option value="DEFAULT" disabled>
-                  Sort
-                </option>
-                <option value="LOW_TO_HIGH">Price Low to High</option>
-                <option value="HIGH_TO_LOW">Price High to Low</option>
-                <option value="RATING">Rating</option>
-              </select>
+              <div className="books__header">
+                <h2 className="section__title books__header--title">All Books</h2>
+                <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
+                  <option value="DEFAULT" disabled>
+                    Sort
+                  </option>
+                  <option value="LOW_TO_HIGH">Price Low to High</option>
+                  <option value="HIGH_TO_LOW">Price High to Low</option>
+                  <option value="RATING">Rating</option>
+                </select>
+              </div>
             </div>
             <div className="books">
               {books.map((book) => (
